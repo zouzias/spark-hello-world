17aea260fb337037a81395fe01e903751b9ffaf5
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2017-03-10 12:34:49 +0100

    update to spark 2.1.0

--- submit-spark-hello-world.sh
+++ submit-spark-hello-world.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Compiling application..."
-sbt package
+sbt assembly
 
 # Directory where spark-submit is defined
 # Install spark from https://spark.apache.org/downloads.html
-SPARK_HOME=/Users/zouzias/spark-1.3.1-bin-hadoop2.6
+SPARK_HOME=/Users/${USER}/spark-2.1.0-bin-hadoop2.7
 
 # JAR containing a simple hello world
-JARFILE=`pwd`/target/scala-2.10/helloworld_2.10-1.0.jar
+JARFILE=`pwd`/target/scala-2.11/helloworld_2.11-1.0.jar
 
 # Run it locally
 ${SPARK_HOME}/bin/spark-submit --class HelloWorld --master local $JARFILE
