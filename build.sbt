8c4804e470925ca5b8a47778c532e74b7ee67191
Anastasios Zouzias <zouzias@gmail.com>
2018-01-29 08:51:10 +0000

    build.sbt: update sparkV 2.2.1

--- build.sbt
+++ build.sbt
@@ -4,5 +4,5 @@ version := "1.0"
  
 scalaVersion := "2.11.8"
  
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.2" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.1" % "provided"
 
--- submit-spark-hello-world.sh
+++ submit-spark-hello-world.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Compiling application..."
+echo "Compiling and assembling application..."
 sbt assembly
 
 # Directory where spark-submit is defined
-# Install spark version 2.1.1 from https://spark.apache.org/downloads.html
-SPARK_HOME=/home/${USER}/spark-2.1.1-bin-hadoop2.7
+# Install spark version 2.2.1 from https://spark.apache.org/downloads.html
+SPARK_HOME=/home/${USER}/spark-2.2.1-bin-hadoop2.7
 
 # JAR containing a simple hello world
 JAR=`ls target/scala-2.11/*.jar`
