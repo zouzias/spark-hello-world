4cdfea1bb8c9182e34fa8eae6aa4bca3afd11287
Anastasios Zouzias <anastasios.zouzias@huawei.com>
2021-09-01 14:40:24 +0200

    [scripts] fix jar location

--- submit-spark-hello-world.sh
+++ submit-spark-hello-world.sh
@@ -5,10 +5,11 @@ sbt assembly
 
 # Directory where spark-submit is defined
 # Install spark from https://spark.apache.org/downloads.html
-SPARK_HOME=/Users/${USER}/spark-2.4.8-bin-hadoop2.7
+# NOTE: you need to change this to your spark installation
+SPARK_HOME=/home/${USER}/spark-3.1.2-bin-hadoop3.2
 
 # JAR containing a simple hello world
-JARFILE=`pwd`/target/scala-2.11/HelloWorld-assembly-1.0.jar
+JARFILE=`pwd`/target/scala-2.12/HelloWorld-assembly-0.1.0.jar
 
 # Run it locally
 ${SPARK_HOME}/bin/spark-submit --class HelloWorld --master local $JARFILE
