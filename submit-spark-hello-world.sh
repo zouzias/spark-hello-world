ba92a75985f791c47ec66b1e8fea0965eee3c4b3
Anastasios Zouzias <zouzias@gmail.com>
2022-11-21 20:51:54 +0100

    update script

--- submit-spark-hello-world.sh
+++ submit-spark-hello-world.sh
@@ -6,7 +6,9 @@ sbt assembly
 # Directory where spark-submit is defined
 # Install spark from https://spark.apache.org/downloads.html
 # NOTE: you need to change this to your spark installation
-SPARK_HOME=/home/${USER}/spark-3.1.2-bin-hadoop3.2
+
+# Replace "home" with "Users" in MacOS
+SPARK_HOME=/home/${USER}/spark-3.3.1-bin-hadoop3
 
 # JAR containing a simple hello world
 JARFILE=`pwd`/target/scala-2.12/HelloWorld-assembly-0.1.0.jar
