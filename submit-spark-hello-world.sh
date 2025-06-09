f7ef86cbebd6bcd0601b5e836b6a62753b025e18
Anastasios Zouzias <zouzias@users.noreply.github.com>
2025-06-09 17:20:34 +0200

    fix (#92)
    Co-authored-by: Anastasios Zouzias <anastasios.zouzias@huawei.com>

--- submit-spark-hello-world.sh
+++ submit-spark-hello-world.sh
@@ -8,7 +8,7 @@ sbt assembly
 # NOTE: you need to change this to your spark installation
 
 # Replace "home" with "Users" in MacOS
-SPARK_HOME=/home/${USER}/spark-3.3.1-bin-hadoop3
+SPARK_HOME=/home/${USER}/spark-3.5.6-bin-hadoop3
 
 # JAR containing a simple hello world
 JARFILE=`pwd`/target/scala-2.12/HelloWorld-assembly-0.1.0.jar
